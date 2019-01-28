bee001a3f660f914a560363c1543979c3edb6fbe
Xi Luo <dycz0fx@gmail.com>
2019-01-28 15:39:46 -0500

    change seg count into seg size in Future Bcast

--- ompi/mca/coll/future/coll_future_component.c
+++ ompi/mca/coll/future/coll_future_component.c
@@ -128,21 +128,21 @@ static int future_register(void)
     cs->future_output = opal_output_open(NULL);
     opal_output_set_verbosity(cs->future_output, coll_future_verbose);
 
-    cs->future_bcast_up_count = 65536;
-    (void) mca_base_component_var_register(c, "bcast_up_count",
-                                           "up level segment count for bcast",
+    cs->future_bcast_up_segsize = 65536;
+    (void) mca_base_component_var_register(c, "bcast_up_segsize",
+                                           "up level segment size for bcast",
                                            MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
                                            OPAL_INFO_LVL_9,
                                            MCA_BASE_VAR_SCOPE_READONLY,
-                                           &cs->future_bcast_up_count);
+                                           &cs->future_bcast_up_segsize);
     
-    cs->future_bcast_low_count = 524288;
-    (void) mca_base_component_var_register(c, "bcast_low_count",
-                                           "low level segment count for bcast",
+    cs->future_bcast_low_segsize = 524288;
+    (void) mca_base_component_var_register(c, "bcast_low_segsize",
+                                           "low level segment size for bcast",
                                            MCA_BASE_VAR_TYPE_INT, NULL, 0, 0,
                                            OPAL_INFO_LVL_9,
                                            MCA_BASE_VAR_SCOPE_READONLY,
-                                           &cs->future_bcast_low_count);
+                                           &cs->future_bcast_low_segsize);
     
     cs->future_allreduce_segsize = 524288;
     (void) mca_base_component_var_register(c, "allreduce_segsize",
